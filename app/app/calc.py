cb1ff0343ac09cb762ec6fb6a6a974566b32534d
Narendra POLASAM <npolasam@yelsterdigital.com>
2022-07-07 17:37:23 +0200

    wait for db changes

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x,y):
+    """Add x and y and return the result"""
+    return x+y;
+
+def substract(x,y):
+    """substract x from y and return restults"""
+    return y-x
